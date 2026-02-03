c580c53db44d3d688f122742144918714999dd90
Jordan <jordan07990@gmail.com>
2026-02-03 21:39:50 +0100

    Añadido blog al menu footer

--- footer.php
+++ footer.php
@@ -3,6 +3,7 @@
     <p style="margin-bottom: 40px; font-size: 0.9rem;">
     <a href="index.php" style="margin: 0 10px;">Inicio</a>
     <a href="oportunidades.php" style="margin: 0 10px;">Oportunidades</a>
+    <a href="blog.php" style="margin: 0 10px;">Blog</a>
     <a href="sobre-nosotros.php" style="margin: 0 10px;">Nosotros</a>
     </p>
     <p>&copy; 2026 Iroa Gestión. Todos los derechos reservados.</p>
