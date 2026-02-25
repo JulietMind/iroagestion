9b2424ffeab0caeca8755b4def0513479f743140
Jordan <jordan07990@gmail.com>
2026-02-25 22:31:33 +0100

    Cambiado titulo pagina finalizadas

--- finalizadas.php
+++ finalizadas.php
@@ -1,7 +1,7 @@
 <?php include 'header.php'; ?>
 <div style="padding: 80px 0; background: #0b1120;">
 <div class="container">
-<h1 style="text-align: center; font-family: 'Playfair Display', serif; color: white; margin-bottom: 50px;">Oportunidades en curso</h1>
+<h1 style="text-align: center; font-family: 'Playfair Display', serif; color: white; margin-bottom: 50px;">Inversiones finalizadas</h1>
 <!-- Contenedor Grid Completo -->
 <div id="catalog-grid" class="project-grid"></div>
 </div>
