f63e41b01fdbdf78aadf4544ef0777e295475b59
Jordan <jordan07990@gmail.com>
2026-02-25 20:24:24 +0100

    Cambiado título de página curso.php

--- curso.php
+++ curso.php
@@ -1,7 +1,7 @@
 <?php include 'header.php'; ?>
 <div style="padding: 80px 0;">
 <div class="container">
-<h1 style="text-align: center; color: white; margin-bottom: 50px;">Blog de Inversión</h1>
+<h1 style="text-align: center; color: white; margin-bottom: 50px;">Inversiones en curso</h1>
 <div id="blog-grid" class="project-grid"></div>
 </div>
 </div>
